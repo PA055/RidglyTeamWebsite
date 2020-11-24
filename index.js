960e1acbf37a5f91a4010de1d061d87cdb0542b0
PA055 <praful.adiga@gmail.com>
2020-11-24 17:08:51 -0500

    fixed a bug

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.onload = function () {
     var array_len = full_screens.length;
     for (var i = 0; i < array_len; i++) {
         console.log(full_screens[i])
-        full_screens[i].style.width = screen.width + "px";
+        // full_screens[i].style.width = screen.width + "px";
         full_screens[i].style.height = screen.height + "px";
     }
 }
