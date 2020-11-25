224de13eb94c0b8a8f08520b1283eca3c5e792f0
PA055 <praful.adiga@gmail.com>
2020-11-25 09:25:00 -0500

    added jquery

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+$( document ).ready(function () {
     var full_screens = document.getElementsByClassName('full-page');
     var array_len = full_screens.length;
     for (var i = 0; i < array_len; i++) {
@@ -6,7 +6,7 @@ window.onload = function () {
         // full_screens[i].style.width = screen.width + "px";
         full_screens[i].style.height = screen.height + "px";
     }
-}
+})
 
 window.addEventListener('scroll', function(){
     if (this.scollY > 467) {
