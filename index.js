ca891b859460f9951f7765901255b732bcfd2fa9
PA055 <praful.adiga@gmail.com>
2020-11-26 09:40:25 -0500

    updated jquery

--- index.js
+++ index.js
@@ -6,7 +6,42 @@ $( document ).ready(function () {
         // full_screens[i].style.width = screen.width + "px";
         full_screens[i].style.height = screen.height + "px";
     }
+    $( "#home-li" ).click(function ( event ) {
+        event.preventDefault();
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    })
+    $( "#our-story-li" ).click(function ( event ){
+        event.preventDefault();
+        targetedScroll('#story')
+        
+    })
+    $( "#competitions-awards-li" ).click(function ( event ){
+        event.preventDefault();
+        targetedScroll('#awards')
+        
+    })
+    $( "#teams-li" ).click(function ( event ){
+        event.preventDefault();
+        targetedScroll('#teams')
+        
+    })
+    $( "#our-program-li" ).click(function ( event ){
+        event.preventDefault();
+        targetedScroll('#program')
+        
+    })
+    $( "#help-us-li" ).click(function ( event ){
+        event.preventDefault();
+        targetedScroll('#help')
+        
+    })
 })
+
+function targetedScroll(id) {
+    $('html, body').animate({
+        scrollTop: $(id).offset().top
+    }, 2000);
+}
 
 /* window.addEventListener('scroll', function(){
     if (this.scollY > 467) {
